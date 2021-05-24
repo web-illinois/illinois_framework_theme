b2b1aeccd66e075f00d462834edb5bcf58f0db2a
David Cross <dcross1@gmail.com>
2021-05-24 13:42:48 -0500

    Individual profile displays - needs to be formatted

--- js/directory.js
+++ js/directory.js
@@ -1,13 +1,13 @@
 var $ = jQuery;
 
 console.log("https://ws.engr.illinois.edu/directory/item.asp?unit=1538&id=" + net_id + "&imgwidth=250&template=310");
-$.ajax({url: "https://ws.engr.illinois.edu/directory/item.asp?unit=1538&id=" + net_id + "&imgwidth=250&template=310", success: function(result) {
+$.ajax({url: "https://ws.engr.illinois.edu/directory/item.asp?unit=1538&id=" + net_id + "&imgwidth=250&template=", success: function(result) {
   result = result.replace(/document.write\(\'/g, '');
   result = result.replace(/\'\);/g, '');
   result = result.replace(/\\/g, '');
   result = result.replace('class="row"', '');
 //  result = result.substring(0, result.indexOf('function ShowTab('));
 
-  document.write(result);
+  $('.directory_output').html(result);
 }});
 
