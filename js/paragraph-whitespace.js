fe653f09b691bb77477833a6b2480a626b3e703f
wrg20 <wrg1025@gmail.com>
2022-05-02 16:19:26 -0500

    Update paragraph-whitespace.js

--- js/paragraph-whitespace.js
+++ js/paragraph-whitespace.js
@@ -1,6 +1,6 @@
 jQuery(function ($) {
-  // find each div with the white background class
-  ($("div").find(".background--color--white")).each(function () {
+  // find each div with the white background class except feature video and feature split
+  ($("div:not(.paragraph--type--feature-split, .paragraph--type--feature-video)").find(".background--color--white")).each(function () {
       /*
          Check to see if the next direct div has the same white background class.
          If so, then reduce the bottom padding on the current div and reduce
