48f2c59d2da36d04ae9a71f89a5b88cf2f89fd9a
wrg20 <wrg1025@gmail.com>
2022-05-02 17:22:32 -0500

    update feature grouped

--- js/paragraph-whitespace.js
+++ js/paragraph-whitespace.js
@@ -1,12 +1,12 @@
 jQuery(function ($) {
-  // find each div with the white background class except feature video and feature split
+  // find each div with the white background class
   ($("div:not(.paragraph--type--feature-split, .paragraph--type--feature-video)").find(".background--color--white")).each(function () {
       /*
          Check to see if the next direct div has the same white background class.
          If so, then reduce the bottom padding on the current div and reduce
          the top padding on the next div with a white background.
       */
-      if($(this).next("div").hasClass("background--color--white")){
+      if($(this).next("div").not(".paragraph--type--feature-split, .paragraph--type--feature-video").hasClass("background--color--white")){
         $(this).addClass('reduce-padding--bottom');
         $(this).next("div").addClass('reduce-padding--top');
       }
