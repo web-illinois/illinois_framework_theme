3dd71d7a82d6ac658f275e8d9ab7f186f0ebfbf6
Natalie Johnstone <nhj@illinois.edu>
2025-07-01 16:30:29 -0500

    updates to paragraphs and buttons

--- js/paragraph-rt.js
+++ js/paragraph-rt.js
@@ -1,3 +1,4 @@
 jQuery(function($) {
   $('ilw-columns .paragraph--type--rt ilw-content[width="auto"]').removeAttr('width');
+  $('ilw-columns .paragraph--type--contact ilw-columns[width="auto"]').removeAttr('width');
 });
