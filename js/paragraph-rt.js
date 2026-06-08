315d9b129c4895cefe02ef4ea5ff1dfdfa846c29
Natalie Johnstone <nhj@illinois.edu>
2026-06-08 14:47:34 -0500

    fixing the value of the background color in twig, removing width=auto and adding fixed-width class

--- js/paragraph-rt.js
+++ js/paragraph-rt.js
@@ -1,7 +1,3 @@
 jQuery(function($) {
-  $('ilw-columns .paragraph--type--rt ilw-content[width="auto"]:not([theme="gray"])').removeAttr('width');
-  $('ilw-columns .paragraph--type--rt ilw-content[width="auto"][theme="gray"]').removeAttr('width').attr('padding', '20px 20px 30px');
-  $('ilw-columns .paragraph--type--rt ilw-columns[width="auto"]').removeAttr('width');
-  $('article.news .paragraph--type--rt ilw-content[width="auto"]').removeAttr('width');//fix for paragraphs in news content type
-  $('article.spotlight .paragraph--type--rt ilw-content[width="auto"]').removeAttr('width');//fix for paragraphs in spotlight content type
+  $('ilw-columns .paragraph--type--rt ilw-content[theme="gray"]').attr('padding', '20px 20px 30px');
 });
